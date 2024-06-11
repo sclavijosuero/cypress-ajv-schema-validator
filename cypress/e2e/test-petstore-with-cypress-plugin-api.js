3a33ddd6d5c7e550b829a12294ec8b5aba493c6e
Sebastian Clavijo Suero (Personal) <128645385+sclavijosuero@users.noreply.github.com>
2024-06-11 14:53:12 -0700

    Add test-petstore-with-cypress-plugin-api.js

---
+++ cypress/e2e/test-petstore-with-cypress-plugin-api.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+// Import cypress-ajv-schema-validator plugin
+import '../../src/index.js'
+
+import 'cypress-plugin-api'
+
+describe('Petstore API', () => {
+
+    var petstoreAPISchema
+
+    beforeEach(() => {
+        cy.api('https://petstore.swagger.io/v2/swagger.json').its('body', { log: false }).then((schema) => {
+            petstoreAPISchema = schema
+        })
+
+    })
+
+    it('should validate the OpenAPI schema', () => {
+        cy.api(
+            { url: 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending', headers: { 'Content-Type': 'application/json' } }).then((response) => {
+                expect(response.status).to.eq(200)
+            }).validateSchema(petstoreAPISchema, { endpoint: '/pet/findByStatus', method: 'get', status: 200 })
+    })
+})
+
