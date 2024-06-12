594377a3ddeddd966ea0c8a80481608d94589401
Sebastian Clavijo Suero (Personal) <128645385+sclavijosuero@users.noreply.github.com>
2024-06-11 17:42:51 -0700

    Slight change in test-petstore-with-cypress-plugin-api.js

--- cypress/e2e/test-petstore-with-cypress-plugin-api.js
+++ cypress/e2e/test-petstore-with-cypress-plugin-api.js
@@ -6,21 +6,22 @@ import '../../src/index.js'
 import 'cypress-plugin-api'
 
 describe('Petstore API', () => {
-
     var petstoreAPISchema
 
     before(() => {
         cy.api('https://petstore.swagger.io/v2/swagger.json').its('body', { log: false }).then((schema) => {
             petstoreAPISchema = schema
         })
-
     })
 
-    it('should validate the OpenAPI schema', () => {
-        cy.api(
-            { url: 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending', headers: { 'Content-Type': 'application/json' } }).then((response) => {
-                expect(response.status).to.eq(200)
-            }).validateSchema(petstoreAPISchema, { endpoint: '/pet/findByStatus', method: 'get', status: 200 })
+    it('should validate the OpenAPI schema for findByStatus', () => {
+        const request = {
+            url: 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending',
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        cy.api(request).then(response => { expect(response.status).to.eq(200) })
+            .validateSchema(petstoreAPISchema, { endpoint: '/pet/findByStatus', method: 'get', status: 200 })
     })
 })
 
